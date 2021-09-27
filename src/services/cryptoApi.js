06718bd203f76d18968305a9c76e2bedd3bc0389
Karen Grigoryan <karen.grigoryan@emergn.com>
2021-09-28 01:31:36 +0300

    store, api

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,20 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+
+const cryptoApiHeaders = {
+  "x-rapidapi-host": "coinranking1.p.rapidapi.com",
+  "x-rapidapi-key": "af7bb21495msh54391993c0c1fbcp16a883jsne8d8141e1790",
+};
+
+const baseUrl = "https://coinranking1.p.rapidapi.com/stats";
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: () => createRequest("/exchanges"),
+    }),
+  }),
+});
